de0b9b401fea94c04a40559a24e2dcd83d55ebfa
Douglas Christopher Wilson <doug@somethingdoug.com>
2022-01-29 19:13:10 -0500

    audio

--- scripts/fetch-iana.js
+++ scripts/fetch-iana.js
@@ -34,7 +34,7 @@ var MIME_TYPE_HAS_CHARSET_PARAMETER_REGEXP = /parameters\s*:[^.]*\bcharset\b/im
 co(function * () {
   var gens = yield [
     get('application', { extensions: /(?:\/(?:ecmascript|express|gzip|(?:ld|manifest)\+json|n-quads|n-triples|pgp-.+|trig|vnd\.(?:age|apple\..+|dbf|mapbox-vector-tile|rar))|\+xml)$/ }),
-    get('audio', { extensions: /\/mobile-xmf$/ }),
+    get('audio', { extensions: true }),
     get('font', { extensions: true }),
     get('image', { extensions: true }),
     get('message', { extensions: true }),
