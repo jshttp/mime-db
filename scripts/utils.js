e7548fb503f2f1015026c09012b47e2abe7a82a6
Sebastian Beltran <bjohansebas@gmail.com>
2025-01-07 10:31:53 -0500

    reduce code

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,8 @@
 const { request } = require('undici')
 
 module.exports.request = async function (url, options) {
-  const res = await request(url, {
+  return request(url, {
     ...options,
     headers: { 'User-Agent': 'node' }
   })
-
-  return res
 }
